99c52b8d9267e017175aeb5b337e00c8b89f423b
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-12 11:23:21 +0200

    ingredient route

--- src/routes/cocktailRoutes.ts
+++ src/routes/cocktailRoutes.ts
@@ -10,7 +10,7 @@ router.get("/cocktail/all", (req: Request, res: Response): void => {
 
 //create cocktail
 
-router.post("/cocktail/create", async (req: Request, res: Response) => {
+router.post("/cocktail/create", async (req: Request, res: Response): Promise<void> => {
   try {
   } catch (error: any) {
     res.status(400).json(error.message);
