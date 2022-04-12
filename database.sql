2b5c0fef8bdde1dbfa59a9db4668ac233a6ff18d
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-12 14:54:14 +0200

    ingredient schema completed

--- database.sql
+++ database.sql
@@ -9,5 +9,17 @@ CREATE TABLE cocktails(
 
 CREATE TABLE ingredients(
     ingredients_id SERIAL PRIMARY KEY,
-    name VARCHAR(50)
+    name VARCHAR(50) UNIQUE,
+    unit VARCHAR(10) NOT NULL,
+    category VARCHAR(20) NOT NULL,
+    isRare BOOLEAN NOT NULL,
+    imgUrl VARCHAR(150)
 );
+
+
+--"id": "sTHzlGIVhBdKcxdf",
+-- "name": "Ligth Rum",
+-- "unit": "cl",
+-- "category": "alcohol",
+-- "isRare": false,
+-- "imgUrl": "https://www.thecocktaildb.com/images/ingredients/Light%20Rum.png"
