1c55cdb35c983cd6b0e84c82a7f02a7c93d954da
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-28 18:30:04 +0200

    commit last changes

--- dist/ingredients/queries.js
+++ dist/ingredients/queries.js
@@ -1,7 +1,7 @@
 "use strict";
 const getAllIngredients = "SELECT * FROM ingredient";
 const getIngredientById = "SELECT * FROM ingredient WHERE ingredient_id = $1";
-const checkIfIngredientExists = "SELECT i FROM ingredient i WHERE LOWER(i.name) = LOWER($1)";
+const checkIfIngredientExists = "SELECT i FROM ingredient i WHERE LOWER(i.ingredient_name) = LOWER($1)";
 const updateIngredientFieldById = "UPDATE ingredient SET $1=$2 WHERE ingredient_id = $3";
 const deleteIngredientById = "DELETE ingredient WHERE ingredient_id = $1";
 module.exports = {
--- schema.sql
+++ schema.sql
@@ -2,22 +2,34 @@ CREATE DATABASE my_bar_db;
 
 --\c into my_bar_DB
 
-CREATE TABLE cocktail(
-    cocktail_id SERIAL PRIMARY KEY,
-    name VARCHAR(50)
-);
+-- TABLE CREATION
 
 CREATE TABLE ingredient(
-    ingredients_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE,
+    ingredient_id SERIAL PRIMARY KEY,
+    ingredient_name VARCHAR(50) UNIQUE,
     unit VARCHAR(10) NOT NULL,
     category VARCHAR(20) NOT NULL,
-    isRare BOOLEAN DEFAULT false,
-    imgUrl VARCHAR(150)
+    is_rare BOOLEAN DEFAULT false,
+    ingredient_img_url TEXT
 );
 
 
+CREATE TABLE cocktail(
+    cocktail_id SERIAL PRIMARY KEY,
+    cocktail_name VARCHAR(100) UNIQUE NOT NULL,
+    glass VARCHAR(50),
+    is_alcoholic BOOLEAN DEFAULT true,
+    is_common BOOLEAN DEFAULT true,
+    cocktail_img_url TEXT
+);
 
+
+CREATE TABLE measure(
+    measure_id SERIAL PRIMARY KEY,
+    ingredients_id ,
+    quantity ,
+    cocktail_id ,
+)
 
 --"id": "sTHzlGIVhBdKcxdf",
 -- "name": "Light Rum",
